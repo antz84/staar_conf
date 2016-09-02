2c7b2a337c19f6e18422114ad594f2c687b9dff6
MacRay <ruixu0530@gmail.com>
2016-09-02 14:52:25 +1000

    boilplate set

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,1 +1,31 @@
+var Router = Backbone.Router.extend({
 
+    routes: {
+      'bookings': 'listTalks',           // #bookings view
+      // '': 'listTalks'                // #
+    },
+
+    listTalks: function() {
+    $.ajax({
+      type: "GET",
+      url: 'http://localhost:3000/api/talks'
+    }).done(
+      function(res) {
+        console.log(res);
+        var talksView = new TalksIndexView();
+        $('.main').html(talksView.render(res));
+      });
+    }
+});
+
+
+// var EventsIndexView = Backbone.View.extend({
+//
+//   template: _.template($('#talks-index-template').html()),
+//
+//   events: {} ,
+//
+//   render: function (res) {
+//       this.$el.html(this.template({talks: res }));
+//       return this.el;}
+// });
