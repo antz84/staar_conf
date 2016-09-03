cf90dffe6eb2f02dd1700042889053b6fcc2a870
Andrew Pratley <pratley.andrew@gmail.com>
2016-09-04 00:05:52 +1000

    slider refactored and stripe partially implemented

--- app/assets/javascripts/booking.js
+++ app/assets/javascripts/booking.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
     $('.button-collapse').sideNav({
       edge: 'right', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
     });
 
 });
