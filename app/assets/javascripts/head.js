2da0e6712cbdf3d75e2a61ffa8ea6f3305cff164
Sam West-Sooby <s.west.sooby@gmail.com>
2016-09-08 15:50:11 +1000

    nav bar fixed

--- app/assets/javascripts/head.js
+++ app/assets/javascripts/head.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
     $('.scrollspy').scrollSpy();
+    openNav();
   });
 
-/* Set the width of the side navigation to 250px */
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "180px";
+    document.getElementById("mySidenav").style.width = "12%";
+    document.querySelector(".container").style.marginLeft = "20%";
 }
 
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
+//========= this is not needed anymore as our nav bar is fixed
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+// function closeNav() {
+//     document.getElementById("mySidenav").style.width = "0";
+//     document.getElementById("main").style.marginLeft = "0";
+// }
